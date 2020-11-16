3ce3d9ca3a108a74bd2d82cf7eef4cec94c89992
Chris Kay <ckay84@protonmail.com>
2020-11-15 23:59:53 -0500

    Add deck and card components

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+  render() {
+    return (
+      <div className="Card">
+        Card
+      </div>
+    )
+  }
+}
+
+export default Card
---
+++ src/components/Deck.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Deck extends Component {
+  render() {
+    return (
+      <div className="Deck">
+        Deck
+      </div>
+    )
+  }
+}
+
+export default Deck
