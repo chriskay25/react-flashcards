438d8988377702651fd52144bfd8866a1ba83f79
Chris Kay <ckay84@protonmail.com>
2020-11-16 12:33:35 -0500

    Add a navbar component with app title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import DeckContainer from './containers/DeckContainer';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <DeckContainer />
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='NavBar'>
+      <h1 className='title'>FLASHCARDS</h1>
+    </div>
+  )
+}
+
+export default NavBar
