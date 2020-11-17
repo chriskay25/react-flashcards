9880f57d18a7c48a4999f887a1a0f6da4fae40b7
Chris Kay <ckay84@protonmail.com>
2020-11-16 19:46:26 -0500

    Create question and answer display components

---
+++ src/components/Answer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Answer = () => {
+  <div className='Answer'>
+    Answer
+  </div>
+}
+
+export default Answer
---
+++ src/components/Question.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Question = () => {
+  return (
+    <div className='Question'>
+      Question
+    </div>
+  )
+}
+
+export default Question
