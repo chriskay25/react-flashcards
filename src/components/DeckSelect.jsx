08dfb440bce7d0f2a0ba1f5ed45c5d4e89f2ccf0
Chris Kay <ckay84@protonmail.com>
2020-11-18 23:52:18 -0500

    Changed select from mui to regular jsx to make customization easier

--- src/components/DeckSelect.jsx
+++ src/components/DeckSelect.jsx
@@ -1,52 +1,28 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button'
+import StyledButton from './StyledButton'
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(2),
-    minWidth: 160,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-export default function SimpleSelect(props) {
-  const classes = useStyles();
-  const [deck, setDeck] = useState('');
+export default function DeckSelect(props) {
+  const [deck, setDeck] = useState(null);
 
   const handleChange = (event) => {
     setDeck(event.target.value);
     console.log("deck is equal to: ", event.target.value)
   };
 
-  const menuItems = () => {
+  const deckOptions = () => {
     return props.decks.map(deck => {
-      return <MenuItem key={deck.id} value={deck.id}>{deck.attributes.name}</MenuItem>
+      return <option value={deck.id}>{deck.attributes.name}</option>
     })
   }
 
   return (
-    <div>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="deck-selection">Deck</InputLabel>
-        <Select
-          labelId="deck-selection"
-          id="deck-select"
-          value={deck}
-          onChange={handleChange}
-        >
-          {menuItems()}
-        </Select>
-        <Button>View Deck</Button>
-      </FormControl>
-    </div>
+    <select onChange={handleChange} value={deck}>
+      {!deck && (
+        <option>Choose a Deck</option>
+      )}
+      {deckOptions()}
+    </select>
   )
   
 }
