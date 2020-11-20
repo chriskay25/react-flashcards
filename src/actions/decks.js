41736014d3ccf312e9c4ca453cd5a15ae7c0661b
Chris Kay <ckay84@protonmail.com>
2020-11-19 23:58:58 -0500

    Async actions to get deck(s)

---
+++ src/actions/decks.js
@@ -0,0 +1,21 @@
+export const getDeck = (id) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/collections/${id}`)
+      .then(resp => resp.json())
+      .then(deck => dispatch({
+        type: 'GET_DECK',
+        payload: deck
+      }))
+  }
+}
+
+export const getDecks = () => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/collections')
+      .then(resp => resp.json())
+      .then(decks => dispatch({
+        type: 'GET_DECKS',
+        payload: decks
+      }))
+  }
+}
