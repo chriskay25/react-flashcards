177bf2afc69c2395002baf582f32a4653a7e2506
Chris Kay <ckay84@protonmail.com>
2020-11-24 23:58:49 -0500

    Correct the payloads

--- src/actions/decks.js
+++ src/actions/decks.js
@@ -2,10 +2,12 @@ export const getDeck = (id) => {
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/collections/${id}`)
       .then(resp => resp.json())
-      .then(deck => dispatch({
-        type: 'GET_DECK',
-        payload: deck
-      }))
+      .then(deck => {
+        dispatch({
+          type: 'GET_DECK',
+          payload: deck.data
+        })
+      })
   }
 }
 
@@ -13,9 +15,11 @@ export const getDecks = () => {
   return (dispatch) => {
     fetch('http://localhost:3000/api/v1/collections')
       .then(resp => resp.json())
-      .then(decks => dispatch({
-        type: 'GET_DECKS',
-        payload: decks
-      }))
+      .then(decks => {
+        dispatch({
+          type: 'GET_DECKS',
+          payload: decks.data
+        })
+      })
   }
 }
