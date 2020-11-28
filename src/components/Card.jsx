5c395b77223a0b728ba969f3872ef17317ea8afc
Chris Kay <ckay84@protonmail.com>
2020-11-27 23:57:33 -0500

    Add framer motion to div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Question from './Question'
 import Answer from './Answer'
+import { motion } from 'framer-motion'
 
 const Card = ({ question, answer, number }) => {
   return (
-    <div className='Card-display'>
+    <motion.div className='Card-display' animate={{ scale: .95 }} transition={{ duration: 1 }}>
       <p><strong>{number + 1}.</strong></p>
       <Question question={question} />
       <Answer answer={answer} />
-    </div>
+    </motion.div>
   )
 }
 
