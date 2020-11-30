4062ad9ccb24815c45f69e11a4b4a035ab4df6bf
Chris Kay <ckay84@protonmail.com>
2020-11-29 23:51:46 -0500

    Update rootReducer w/ cardReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import { deckReducer } from './decks'
+import { cardReducer } from './cards'
 import { cards } from './cards'
 
 export default combineReducers({
-  deckReducer
+  deckReducer,
+  cardReducer
 })
