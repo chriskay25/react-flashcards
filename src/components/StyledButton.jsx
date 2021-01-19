45af18a63ad940487fe92dd05cb4e20107766c2e
Chris Kay <ckay84@protonmail.com>
2021-01-18 19:18:04 -0500

    Delete StyledButton

--- src/components/StyledButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StyledButton = React.forwardRef((props, ref) => (
-  <button ref={ref} className='StyledButton'>
-    {props.children}
-  </button>
-))
-
-export default StyledButton
