52079767333ebb546292d42e621625a0c1fd23b1
Chris Kay <ckay84@protonmail.com>
2021-01-20 02:28:16 -0500

    Add Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import DeckContainer from './containers/DeckContainer';
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import Deck from './components/Deck'
+import { Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Home />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/deck/:id' component={Deck} />
+        <Route exact path='/deck' component={Deck} />
+      </Switch>
+        
     </div>
   );
 }
