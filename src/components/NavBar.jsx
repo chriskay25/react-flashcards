e145fe96126d9145a7da18b455a84a2bcd5422ec
Chris Kay <ckay84@protonmail.com>
2021-01-20 02:17:02 -0500

    Add links and divs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className='NavBar'>
-      <h1 className='title'>FLASHCARDS</h1>
+      <div className='app-title'>
+        <h1 className='title'>FLASHCARDS</h1>
+      </div>
+      <div className='nav-links'>
+        <Link to='/' className='link'>Home</Link>
+        <Link to='/deck' className='link'>Deck</Link>
+      </div>
     </div>
   )
 }
