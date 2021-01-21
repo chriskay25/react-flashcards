eda7cf4c9c872c502db92d221672bbc22ae1b7a7
Chris Kay <ckay84@protonmail.com>
2021-01-21 12:09:59 -0500

    Display Cards differently depending on mode selected in Deck

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react'
 import Card from '../components/Card'
 
-const CardContainer = ({ cards }) => {
+const CardContainer = ({ cards, mode }) => {
+
+  const [index, setIndex] = useState(0)
+
+  const studyMode = () => {
+    return cards.map((card) => {
+      return <Card key={card.id} question={card.question} answer={card.answer} />
+    })
+  }
+
+  const quizMode = () => {
+    const quizCard = cards[index]
+    return <Card key={quizCard} question={quizCard.question} answer={quizCard.answer} />
+  }
 
   return(
     <div className='card-container'>
-      Cards
+      Mode: {mode}
+      <div>
+        {mode === 'Study' && studyMode()}
+      </div>
+      <div>
+        {mode === 'Quiz' && quizMode()}
+      </div>
     </div>
   )
 }
