9c0a7bfd6843d280277f7f4013c113930df4bcee
Chris Kay <ckay84@protonmail.com>
2021-01-28 22:56:43 -0500

    Convert Home to a functional component, add HomeDeckButton, implement motion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,56 @@
 import React, { Component } from 'react'
-import DeckSelect from './DeckSelect'
-import { getDeck, getDecks } from '../actions/deckActions'
-import { connect } from 'react-redux'
+import { motion } from 'framer-motion'
+import HomeDeckButton from './HomeDeckButton'
+import { Link } from 'react-router-dom'
 
-class Home extends Component {
-
-  selectedDeck = (id) => {
-    this.props.getDeck(id)
-  }
-
-  render() {
-    return (
-      <div className='Homepage'>
-        <h2>Welcome! What would you like to study?</h2>
-        <DeckSelect decks={this.props.decks} selectedDeck={this.selectedDeck} />
-      </div>
-    )
-  }
-
-  componentDidMount() {
-    this.props.getDecks()
+const homepageVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+  exit: {
+    x: '-100vw',
+    transition: {
+      duration: 1,
+      when: 'afterChildren'
+    }
   }
 }
 
-const mapState = (state) => ({
-  deck: state.deckReducer.deck,
-  decks: state.deckReducer.decks
-})
+const questionVariants = {
+  hidden: { 
+    x: '-100vw',
+    opacity: 0,
+    fontSize: '2.3rem',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    marginTop: '3rem'
+  },
+  visible: { 
+    x: '3rem', 
+    opacity: 1,
+    textDecoration: 'underline',
+    marginBottom: '50px',
+    transition: { delay: 4, type: 'spring' }
+  },
+  exit: { x: '-100vw' }
+}
 
-export default connect(
-  mapState,
-  { getDeck, getDecks }
-)(Home)
+const Home = () => {
+
+  return (
+      <motion.div className='Homepage' variants={homepageVariants} initial='hidden' animate='visible' whileHover='hover' exit='exit'>
+        <motion.div 
+          variants={questionVariants} 
+          initial='hidden' 
+          animate='visible' 
+          exit='exit'
+        >
+          Welcome! What would you like to study?
+        </motion.div>
+        <Link to='/decks'>
+          <HomeDeckButton />
+        </Link>
+      </motion.div>
+  )
+}
+
+export default Home
