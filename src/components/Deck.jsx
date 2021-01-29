176349c55128c35f55608292efaa92e8509e74d9
Chris Kay <ckay84@protonmail.com>
2021-01-29 00:01:22 -0500

    Convert Deck to class comp, add motion

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,35 +1,72 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import CardContainer from '../containers/CardContainer'
+import { getDeck } from '../actions/deckActions'
 import { connect } from 'react-redux'
+import { motion } from 'framer-motion'
 
-const Deck = ({ deck }) => {
-  const [mode, setMode] = useState('')
-  console.log('deck: ', deck)
+const buttonVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: .3,
+      type: 'spring'
+    }
+  },
+  whileHover: {
+    scale: .9,
+  }
+}
 
-  const handleClick = (e) => {
-    console.log(e)
-    setMode(e.target.innerHTML)
+class Deck extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mode: '',
+    }
   }
 
-  return (
-    <div className="Deck-display">
-      <h1>{deck.attributes.name}</h1>
-      {!mode && <p className='mode-select'>
-        Mode: 
-        <button onClick={handleClick}>Study</button> 
-        <button onClick={handleClick}>Quiz</button>
-      </p>}
-      <p>Cards in Deck: {deck.attributes.cards.length}</p>
+  handleClick = (e) => {
+    e.preventDefault()
+    this.setState({
+      mode: e.target.innerHTML
+    })
+  }
 
-      <CardContainer cards={deck.attributes.cards} mode={mode} />
-    </div>
-  )
+  render() {
+    const { mode } = this.state
+    const { deck } = this.props
+    return (
+      <div className="deck-display">
+        {deck && <div className='deck-title'>
+          <h2>{deck.attributes.name}</h2>
+          <p>- {deck.attributes.cards.length} Cards</p>
+          
+        </div>}
+
+        {!mode && <span className='mode-select'>
+          <motion.button onClick={this.handleClick} variants={buttonVariants} initial='hidden' animate='visible' whileHover='whileHover' whileTap='whileTap'>Study</motion.button> 
+          <motion.button onClick={this.handleClick} variants={buttonVariants} initial='hidden' animate='visible' whileHover='whileHover'>Quiz</motion.button> 
+        </span>}
+
+        {mode && <CardContainer cards={deck.attributes.cards} mode={mode} />}
+      </div>
+    )
+  }
+  componentDidMount() {
+    const id = this.props.match.params.id
+    this.props.getDeck(id)
+  }
 }
 
 const mapState = (state) => ({
-  deck: state.deckReducer.deck,
+  deck: state.deckReducer.deck
 })
 
 export default connect(
-  mapState
+  mapState, { getDeck }
 )(Deck)
