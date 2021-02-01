04d22537663788763dfc264eb4c31a34a4dbe534
Chris Kay <ckay84@protonmail.com>
2021-02-01 18:41:03 -0500

    Adjust motion parameters, remove unused link

--- src/components/DeckOption.jsx
+++ src/components/DeckOption.jsx
@@ -1,33 +1,28 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { Link, Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 const DeckOption = ({ deckId, deckName, index }) => {
     const optionVariants = {
         hidden: {
-            scale: .5,
-            x: '100vw',
+            scaleX: 0,
+            y: '10vh',
         },
         visible: {
-            scale: 1,
-            backgroundColor: '#f08080',
-            x: 0,
-            borderRadius: '5%',
+            scaleX: 1,
+            y: 0,
             transition: {
                 type: 'spring',
                 damping: 15,
-                // mass: 6,
-                delay: index/6,
+                delay: index/8,
             }
         },
         hover: {
             scale: .9,
             boxShadow: '0px 0px 10px #000',
-            // backgroundColor: '#7FFFD4',
-            // backgroundImage: 'radial-gradient(rgb(0,255,255), rgb(0,0,0))',
             transition: {
                 type: 'spring',
-                bounce: .6
+                bounce: .3
             }
         },
         exit: { x: '-100vw' }
@@ -48,9 +43,7 @@ const DeckOption = ({ deckId, deckName, index }) => {
             onClick={handleClick}
         >
             {redirected && <Redirect to={`/decks/${deckId}`} />}
-            {/* <Link to={`/decks/${deckId}`}> */}
-                <h2 style={{color: '#000'}} className='deck-option-name'>{deckName}</h2>
-            {/* </Link> */}
+            <h2 style={{color: '#000'}} className='deck-option-name'>{deckName}</h2>
         </motion.div>
     )
 }
