f9814deb1f627ea37c2a275896d53c3e3a21c86c
Chris Kay <ckay84@protonmail.com>
2021-02-01 18:39:05 -0500

    Add motion and AnimatePresence so cards can cycle

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import Question from './Question'
 import Answer from './Answer'
 import StyledCard from '../styledComponents/StyledCard'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Card = ({ question, answer, index, mode, next, back }) => {
   return (
-    <StyledCard>
-      <p className='question-number'><strong>{index + 1}.</strong></p>
-      <Question question={question} />
-      <Answer answer={answer} />
-      <button className='back-button' onClick={back}>Back</button>
-      <button className='next-button' onClick={next}>Next</button>
-    </StyledCard>
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={index}
+        initial={{ x: '100vw', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{duration: .6, ease: 'easeInOut'}}
+        exit={{ x: '-100vw', opacity: 0.5 }}
+      >
+        <StyledCard>
+          <span className='question-number'><strong>{index + 1}.</strong></span>
+          <Question question={question} />
+          <Answer answer={answer} />
+          <button className='back-button' onClick={back}>Back</button>
+          <button className='next-button' onClick={next}>Next</button>
+        </StyledCard>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
