8fe240416e240e9116b865e0e034a098c7975ff4
Chris Kay <ckay84@protonmail.com>
2021-02-01 18:36:17 -0500

    Adjust motion parameters for HomeDeckButton

--- src/components/HomeDeckButton.jsx
+++ src/components/HomeDeckButton.jsx
@@ -3,17 +3,19 @@ import { motion } from 'framer-motion'
 
 const buttonVariants = {
     hidden: { 
+        opacity: 0,
         scale: 1.1, 
         borderRadius: '20%', 
         y: '10vh',
         boxShadow: '0px 0px 50px #000',
     },
     visible: {
+        opacity: 1,
         scale: 1,
-        backgroundImage: 'radial-gradient(rgb(240,128,128), rgb(240,240,128))',
+        backgroundImage: 'radial-gradient(rgb(240,240,128), rgb(240,128,128))',
         boxShadow: '0px 0px 15px #000',
         transition: {
-            delay: .3,
+            delay: .8,
             type: 'spring', 
             stiffness: 100,
             damping: 14,
@@ -22,16 +24,28 @@ const buttonVariants = {
     },
     hover: { 
         scale: 1.1,
+        backgroundImage: 'radial-gradient(rgb(240,128,128), rgb(240,240,128))',
         boxShadow: '0px 0px 35px #000',
         transition: { 
             type: 'spring',
             stiffness: 100,
             damping: 14,
-            mass: 5,
+            mass: 3,
+        }
+    },
+    tap: {
+        scale: 1,
+        backgroundImage: 'radial-gradient(rgb(240,240,128), rgb(240,128,128))',
+        boxShadow: '0px 0px 10px #000',
+        transition: {
+            type: 'spring',
+            stiffness: 100,
+            damping: 5,
+            mass: 1
         }
     },
     exit: {
-        scale: 0,
+        x: '-100vw',
         transition: { type: 'spring' }
     }
   }
@@ -41,6 +55,7 @@ const HomeDeckButton = () => (
         initial='hidden'
         animate='visible'
         whileHover='hover'
+        whileTap='tap'
         exit='exit'
         variants={buttonVariants}
     >
