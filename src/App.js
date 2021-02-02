aa79c75a815276141593b8a54ba8ec6117e9c626
Chris Kay <ckay84@protonmail.com>
2021-02-02 13:25:33 -0500

    Add login and signup routes, replace home w/ landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Deck from './components/Deck'
 import Decks from './components/Decks'
+import LandingPage from './components/LandingPage'
+import LoginForm from './components/LoginForm'
 import { Route, Switch, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 
@@ -12,13 +14,15 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <AnimatePresence exitBeforeEnter>
+      {/* <AnimatePresence exitBeforeEnter> */}
         <Switch location={location} key={location.pathname}>
           <Route exact path='/decks/:id' component={Deck} />
           <Route exact path='/decks' component={Decks} />
-          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={LoginForm}/>
+          <Route exact path='/signup' />
+          <Route exact path='/' component={LandingPage} />
         </Switch>
-      </AnimatePresence>
+      {/* </AnimatePresence> */}
     </div>
   );
 }
