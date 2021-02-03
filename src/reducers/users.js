e2602ddc00d4bf3b6f8c17b6633b8cb0152bedc2
Chris Kay <ckay84@protonmail.com>
2021-02-03 18:40:17 -0500

    Add action types for logging out and setting currentUser to reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,21 @@
 export const userReducer = (state = {currentUser: null}, action) => {
     switch (action.type) {
-      case 'LOGIN':
-        return {
-          ...state,
-          currentUser: action.payload
-        }
-      default:
-        return state;
+        case 'GET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        case 'LOGIN':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        case 'LOGOUT':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+          return state;
     }
 }
