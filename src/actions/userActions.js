8cec5e24ad4ced2ceb6a59dd4bc27ba37069bc6f
Chris Kay <ckay84@protonmail.com>
2021-02-03 18:39:09 -0500

    Add actions to log user out and to determine current lggd in user

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -25,3 +25,40 @@ export const userLogin = (data) => {
             })
     }
 }
+
+export const userLogout = () => {
+    localStorage.clear()
+    return (dispatch) => {
+      dispatch({
+        type: 'LOGOUT',
+        payload: null 
+      })
+    }
+}
+
+export const getCurrentUser = () => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/auth', {
+          headers: {
+            'Authorization': token,
+          }
+        })
+          .then(resp => resp.json())
+          .then(user => {
+              console.log('GET_C_U action - w/token - user payload: ', user)
+            if (user.error) {
+              alert(user.error)
+            } else {
+              dispatch({type: 'GET_CURRENT_USER', payload: user})
+            }
+          })
+      }
+    } else {
+        console.log('GET_C_U action - no token found :(')
+      return (dispatch) => {
+        dispatch({type: 'GET_CURRENT_USER', payload: null})
+      }
+    }
+  }
