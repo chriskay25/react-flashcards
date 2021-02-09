b071928edea345f590e5f066b445746a2fc3bdfe
Chris Kay <ckay84@protonmail.com>
2021-02-08 23:35:10 -0500

    Convert from class to functional component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,46 +1,38 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { userLogin } from '../actions/userActions'
 
-class LoginForm extends Component {
-    state = {
-        username: '',
-        password: ''
+const LoginForm = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value)
     }
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
     }
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
-        this.props.userLogin(this.state)
-        this.setState({
-            username: '',
-            password: ''
-        })
+        dispatch(userLogin({ username, password }))
     }
 
-    render() {
-        const { username, password } = this.state
-        return (
-            <div>
-                <form className='auth-form' onSubmit={this.handleSubmit}>
-                    <label htmlFor='username'>Username: </label>
-                    <input type='text' name='username' value={username} placeholder='Username' onChange={this.handleChange} />
-                        <br />
-                    <label htmlFor='password'>Password: </label>
-                    <input type='text' name='password' value={password} placeholder='Password' onChange={this.handleChange} />
-                        <br />
-                    <input type='submit' value='Login' />
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form className='auth-form' onSubmit={handleSubmit}>
+                <label htmlFor='username'>Username: </label>
+                <input type='text' name='username' value={username} placeholder='Username' onChange={handleUsernameChange} />
+                    <br />
+                <label htmlFor='password'>Password: </label>
+                <input type='text' name='password' value={password} placeholder='Password' onChange={handlePasswordChange} />
+                    <br />
+                <input type='submit' value='Login' />
+            </form>
+        </div>
+    )
 }
 
-export default connect(
-    null, { userLogin }
-)(LoginForm)
+export default LoginForm
