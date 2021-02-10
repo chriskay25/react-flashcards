da02bbcd537282bceea1bb2395103bff0d7a4a5c
Chris Kay <ckay84@protonmail.com>
2021-02-10 11:57:55 -0500

    Check for currentUser and redirect accordingly, add SignupForm

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
+import React, { useEffect } from 'react'
 import './App.css';
 import NavBar from './components/NavBar'
-import Home from './components/Home'
 import Deck from './components/Deck'
 import Decks from './components/Decks'
 import LandingPage from './components/LandingPage'
 import LoginForm from './components/LoginForm'
-import { Route, Switch, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import SignupForm from './components/SignupForm'
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { getCurrentUser } from './actions/userActions'
+// import { render } from '@testing-library/react';
+// import { AnimatePresence } from 'framer-motion'
 
 
-function App() {
+const App = () => {
+  const currentUser = useSelector(state => state.userReducer.currentUser)
+  const dispatch = useDispatch()
   const location = useLocation()
+
+  useEffect(() => {
+    dispatch(getCurrentUser())
+    console.log('running')
+  }, [dispatch])
+
   return (
     <div className="App">
       <NavBar />
       {/* <AnimatePresence exitBeforeEnter> */}
         <Switch location={location} key={location.pathname}>
-          <Route exact path='/decks/:id' component={Deck} />
-          <Route exact path='/decks' component={Decks} />
-          <Route exact path='/login' component={LoginForm}/>
-          <Route exact path='/signup' />
-          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/decks/:id'>
+            {currentUser ? <Deck /> : <Redirect to='/' />}
+          </Route> 
+          <Route exact path='/decks'>
+            {currentUser ? <Decks /> : <Redirect to='/' />}
+          </Route>
+          <Route exact path='/login'>
+            {currentUser ? <Redirect to='/decks' /> : <LoginForm />}
+          </Route> 
+          <Route exact path='/signup'>
+            {currentUser ? <Redirect to='/decks' /> : <SignupForm />}
+          </Route> 
+          <Route exact path='/'>
+            {currentUser ? <Redirect to='/decks' /> : <LandingPage />}
+          </Route>
         </Switch>
       {/* </AnimatePresence> */}
     </div>
   );
 }
 
-export default App;
+export default App
