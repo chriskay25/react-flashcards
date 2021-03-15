3d42be3bf58f8db565d7213ad2e0f4814a454754
Chris Kay <ckay84@protonmail.com>
2021-03-15 02:23:31 -0400

    Remove labels

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -24,12 +24,8 @@ const LoginForm = () => {
         <div>
             <form className='auth-form' onSubmit={handleSubmit}>
                 <h2>LOGIN</h2>
-                <label htmlFor='username'>Username: </label>
                 <input type='text' name='username' value={username} placeholder='Username' onChange={handleUsernameChange} />
-                    <br />
-                <label htmlFor='password'>Password: </label>
                 <input type='text' name='password' value={password} placeholder='Password' onChange={handlePasswordChange} />
-                    <br />
                 <input className='form-button' type='submit' value='Submit' />
             </form>
         </div>
