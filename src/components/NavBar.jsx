7361e80fe1284fb1c9b4819c70edd55c3b3d60b6
Chris Kay <ckay84@protonmail.com>
2021-03-15 10:20:58 -0400

    Incorporate menu into NavBar, add motion, logout functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+// import { Link, Redirect } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouseUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { useSelector } from 'react-redux'
+import { motion } from 'framer-motion'
+import { useDispatch } from 'react-redux'
+import { userLogout } from '../actions/userActions'
+import Menu from './Menu'
+
+const navBarVariants = {
+  hidden: {
+    height: '0px',
+  },
+  visible: {
+    height: '5rem',
+    // backgroundColor: 'var(--lightred)',
+    justifyContent: 'center',
+  },
+  loggedIn: {
+    height: '2.5rem',
+    // backgroundColor: 'none',
+    justifyContent: 'left',
+  }
+}
+
+const titleVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    fontSize: '3rem',
+  },
+  loggedIn: {
+    fontSize: '1.5rem',
+  }
+}
 
 const NavBar = () => {
+  const currentUser = useSelector(state => state.userReducer.currentUser)
+  const dispatch = useDispatch()
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const logoutUser = () => {
+    dispatch(userLogout())
+  }
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <div className='NavBar'>
+    <motion.div className='NavBar' initial='hidden' animate={currentUser ? 'loggedIn' : 'visible'} variants={navBarVariants}>
       <div className='app-title'>
-        <h1 className='title'>FLASHCARDS</h1>
+        <motion.h1 className='title' initial='hidden' animate={currentUser ? 'loggedIn' : 'visible'} variants={titleVariants}>FLASHCARDS</motion.h1>
       </div>
-      <div className='nav-links'>
-        <Link to='/' className='link'>
-          <FontAwesomeIcon icon={faHouseUser} size='2x' />
-        </Link>
+      <div className='menu-container'>
+        <div className='fontawesome-bars' onClick={handleMenuClick}>
+          {menuOpen ? <FontAwesomeIcon icon={faTimes} size='2x' /> : <FontAwesomeIcon icon={faBars} size='2x' />}
+        </div>
+        <Menu open={menuOpen} logoutUser={logoutUser} />
       </div>
-    </div>
+    </motion.div>
   )
 }
 
