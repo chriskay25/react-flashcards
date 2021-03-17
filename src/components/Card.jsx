4dfecd4c61c84cd194d5de97b26f0d407169ed1e
Chris Kay <ckay84@protonmail.com>
2021-03-16 23:59:07 -0400

    UseState and useEffect to change background based on answers

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Question from './Question'
 import Answer from './Answer'
 import StyledCard from '../styledComponents/StyledCard'
 import { motion, AnimatePresence } from 'framer-motion'
 
 const Card = ({ question, answer, index, mode, next, back }) => {
+  const [isCorrect, setIsCorrect] = useState(null)
+
+  useEffect(() => {
+    setIsCorrect(null)
+  },[index])
+
+  // const handleAnswerChange = (e) => {
+  //   setUserAnswer(e.target.value)
+  // }
+
+  // const handleAnswerSubmit = (e) => {
+  //   e.preventDefault()
+  //   console.log('user answered: ', userAnswer)
+  // }
+
   return (
     <AnimatePresence exitBeforeEnter>
       <motion.div
@@ -14,12 +29,10 @@ const Card = ({ question, answer, index, mode, next, back }) => {
         transition={{duration: .6, ease: 'easeInOut'}}
         exit={{ x: '-100vw', opacity: 0.5 }}
       >
-        <StyledCard>
+        <StyledCard correct={isCorrect}>
           <span className='question-number'><strong>{index + 1}.</strong></span>
           <Question question={question} />
-          <Answer answer={answer} />
-          <button className='back-button' onClick={back}>Back</button>
-          <button className='next-button' onClick={next}>Next</button>
+          <Answer answer={answer} mode={mode} setIsCorrect={setIsCorrect} /> 
         </StyledCard>
       </motion.div>
     </AnimatePresence>
