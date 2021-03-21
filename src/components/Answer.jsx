186a863826d5a003eecd66c3b1d1d9fdeadaf4bd
Chris Kay <ckay84@protonmail.com>
2021-03-21 00:37:08 -0400

    Use state to save answer and disable bttn, shows input if quiz mode, use motion

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 
-const Answer = ({ answer }) => {
+const answerVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  }
+}
+
+const Answer = ({ answer, mode, setIsCorrect }) => {
+  const [userAnswer, setUserAnswer] = useState('')
+  const [disabled, setDisabled] = useState(false)
+
+  const handleAnswerChange = (e) => {
+    setUserAnswer(e.target.value)
+  }
+
+  const handleAnswerSubmit = (e) => {
+    answer === userAnswer ? setIsCorrect(true) : setIsCorrect(false)
+    setDisabled(true)
+  }
+
   return (
-    <div className='Answer'>
-      {answer}
-    </div>
+    <motion.div className='answer' variants={answerVariants} initial='hidden' animate='visible'>
+      {mode === 'Quiz' ? <input type='text' value={userAnswer} onChange={handleAnswerChange} placeholder='Answer'></input> : answer}
+      <button className='answer-submit' onClick={handleAnswerSubmit} disabled={disabled}>Submit</button>
+    </motion.div>
   )
 }
 
