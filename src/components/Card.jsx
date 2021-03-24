81ca2df26af26993c08e26d5c93b21e7abac5477
Chris Kay <ckay84@protonmail.com>
2021-03-23 23:59:41 -0400

    Add Checkmark to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,14 @@ import Question from './Question'
 import Answer from './Answer'
 import StyledCard from '../styledComponents/StyledCard'
 import { motion } from 'framer-motion'
+import Checkmark from './Checkmark'
 
 const Card = ({ card, index, mode }) => {
   const [isCorrect, setIsCorrect] = useState(null)
 
   return (
       <motion.div
+        className='card'
         key={index}
         initial={{ x: '100vw', opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
@@ -19,6 +21,9 @@ const Card = ({ card, index, mode }) => {
           <span className='question-number'><strong>{index + 1}.</strong></span>
           <Question question={card.question} />
           <Answer answer={card.answer} mode={mode} setIsCorrect={setIsCorrect} /> 
+          <motion.div className='checkmark-div' initial={{scaleY: 0}} animate={{scaleY: 1}}>
+            {isCorrect && <Checkmark />}
+          </motion.div>
         </StyledCard>
       </motion.div>
   )
