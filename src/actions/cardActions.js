dc00dd1a7fc15e2a947d42134d1ed5b0b43f71ec
Chris Kay <ckay84@protonmail.com>
2021-06-25 15:27:38 -0400

    Create an action for adding a card

--- src/actions/cardActions.js
+++ src/actions/cardActions.js
@@ -10,3 +10,29 @@ export const getCard = (id) => {
       })
   }
 }
+
+export const addCard = (data) => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/cards', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        card: data
+      })
+    })
+      .then(resp => resp.json())
+      .then(cardData => {
+        console.log('cardData: ', cardData)
+        if (cardData.error) {
+            alert('Invalid Credentials')
+        } else {
+            dispatch({
+                type: 'ADD_CARD',
+                payload: cardData
+            })
+        }
+      })
+  }
+}
