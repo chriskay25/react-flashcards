5c47b2f6cd51ec0999827f0f6ae659353acb5739
Chris Kay <ckay84@protonmail.com>
2021-06-25 15:18:01 -0400

    Add bttn that requires additional click to choose deck

--- src/components/DeckOption.jsx
+++ src/components/DeckOption.jsx
@@ -1,56 +1,57 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { Redirect, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-const DeckOption = ({ deckId, deckName, index }) => {
+const DeckOption = ({ deck }) => {
     const optionVariants = {
         hidden: {
-            marginTop: '.8rem',
-            width: '40%',
-            scaleX: 0,
-            x: '100vw',
         },
         visible: {
-            x: 0,
-            padding: '1rem',
-            scaleX: 1,
-            backgroundColor: 'var(--dark)',
+            width: '50%',
             color: 'var(--lightblue)',
             boxShadow: '0 0 0 0px var(--lightcoral)',
-            transition: {
-                type: 'spring',
-                damping: 15,
-                delay: index/8,
-            }
         },
-        hover: {
-            boxShadow: '0 0 3px 2px var(--lightcoral)',
-            originX: 0,
-            width: '70%',
+        open: {
+            width: '100%',
             color: 'var(--lightcoral)',
+            boxShadow: '0 0 3px 2px var(--lightcoral)',
             transition: {
                 type: 'spring',
                 stiffness: 175,
+                damping: 25,
             }
         },
         exit: { x: '-100vw' }
     }
 
     const [redirected, setRedirected] = useState(false)
+    const [open, setOpen] = useState(false)
     const location = useLocation()
     const locationState = JSON.stringify({ from: location })
 
     return (
         <motion.div className='deck-option' 
             initial='hidden' 
-            animate='visible' 
-            whileHover='hover' 
+            animate={open ? 'open' : 'visible'}
             exit='exit'
             variants={optionVariants}
-            onClick={ () => setRedirected(!redirected) }
+            onClick={ () => setOpen(!open) }
         >
-            {redirected && <Redirect to={{ pathname: `/decks/${deckId}`, state: locationState }} />}
-            <motion.h2 className='deck-option-name'>{deckName}</motion.h2>
+            {redirected && <Redirect to={{ pathname: `/decks/${deck.id}`, state: locationState }} />}
+            <h2 className='deck-option-name'>{deck.name}</h2>
+            {open && <div>{deck.cards.length} Cards</div>}
+            {open && 
+                <motion.button 
+                    className='deck-option-bttn' 
+                    animate={{opacity: 1}} 
+                    transition={{delay: .4}}
+                    onClick={ () => setRedirected(!redirected) }
+                >
+                    <FontAwesomeIcon icon={faArrowRight} size={'2x'} />
+                </motion.button>
+            }
         </motion.div>
     )
 }
