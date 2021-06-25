1f7655e6683e17466c50bab7cafb4f75aa7b6655
Chris Kay <ckay84@protonmail.com>
2021-06-25 15:08:37 -0400

    Add CardNav and variants so cards enter/exit correctly

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,37 +2,67 @@ import React, { useState, useEffect } from 'react';
 import Question from './Question'
 import Answer from './Answer'
 import StyledCard from '../styledComponents/StyledCard'
+import CardNav from './CardNav'
 import { motion } from 'framer-motion'
 import Hint from './Hint'
-import Checkmark from './Checkmark'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 
-const Card = ({ card, index, mode }) => {
-  const [isCorrect, setIsCorrect] = useState(null)
+const cardVariants = {
+  enterRight: {
+    opacity: 0,
+    x: '60vw',
+    scale: .4,
+  },
+  enterLeft: {
+    opacity: 0,
+    x: '-60vw',
+    scale: .4,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    scale: 1,
+  },
+  exitRight: {
+    x: '60vw',
+    opacity: .2,
+    scale: 0,
+  },
+  exitLeft: {
+    x: '-60vw',
+    opacity: .2,
+    scale: 0,
+  }
+}
+
+const Card = ({ card, index, prevIndex, mode, back, next, cardCount }) => {
+  const [correct, setCorrect] = useState(null)
+  const [answered, setAnswered] = useState(false)
   const [hintVisible, setHintVisible] = useState(false)
+  const [direction, setDirection] = useState('forward')
 
   return (
       <motion.div
         className='card'
         key={index}
-        initial={{ x: '100vw', opacity: 0, scale: .2 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{duration: .6, ease: 'easeInOut'}}
-        exit={{ x: '-100vw', opacity: 0.5, scale: .2 }}
+        variants={cardVariants}
+        initial={index > prevIndex ? 'enterRight' : 'enterLeft'}
+        animate='visible'
+        transition={{type: 'spring', stiffness: 175, damping: 25}}
+        exit={direction === 'forward' ? 'exitLeft' : 'exitRight'}
       >
-        <StyledCard correct={isCorrect}>
+        <StyledCard correct={correct}>
           <span className='question-number' style={{fontFamily: 'Bungee', color: 'var(--red'}}><strong>{index + 1}.</strong></span>
           <Question question={card.question} />
-          <Answer answer={card.answer} mode={mode} setIsCorrect={setIsCorrect} /> 
+          <Answer answer={card.answer} mode={mode} setIsCorrect={setCorrect} /> 
           <div className='hint-container'>
             <FontAwesomeIcon onClick={() => setHintVisible(!hintVisible)} className='fontawesome-lightbulb' icon={faLightbulb} size='2x' />
             <Hint hint={card.hint} open={hintVisible} />
           </div>
-          <motion.div className='checkmark-div' initial={{scaleY: 0}} animate={{scaleY: 1}}>
-            {isCorrect && <Checkmark />}
-          </motion.div>
         </StyledCard>
+
+        <CardNav back={back} next={next} cardCount={cardCount} setDirection={setDirection} />
       </motion.div>
   )
 }
