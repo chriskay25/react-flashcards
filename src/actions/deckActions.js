167e2f7cdbb2f0c5366dcd408c995f1c0c721179
Chris Kay <ckay84@protonmail.com>
2021-06-25 15:26:52 -0400

    Create an action for adding a deck

--- src/actions/deckActions.js
+++ src/actions/deckActions.js
@@ -16,11 +16,36 @@ export const getDecks = () => {
     fetch('http://localhost:3000/api/v1/decks')
       .then(resp => resp.json())
       .then(decks => {
-        console.log("getDecks action: ", decks)
         dispatch({
           type: 'GET_DECKS',
           payload: decks
         })
+      })
+  }
+}
+
+export const addDeck = (data) => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/decks', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        deck: data
+      })
+    })
+      .then(resp => resp.json())
+      .then(deckData => {
+        console.log('cardData: ', deckData)
+        if (deckData.error) {
+            alert('Invalid Credentials')
+        } else {
+            dispatch({
+                type: 'ADD_CARD',
+                payload: deckData
+            })
+        }
       })
   }
 }
