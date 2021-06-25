0391777ddfcb6711f9173c49502dc9bb5530256d
Chris Kay <ckay84@protonmail.com>
2021-06-25 15:06:02 -0400

    Simplify CardContainer, move nav inside of card

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,44 +1,27 @@
 import React from 'react'
 import Card from '../components/Card'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+// import CardNav from '../components/CardNav'
 import { AnimatePresence } from 'framer-motion'
 
-const CardContainer = ({ card, index, mode, next, back, cardsInDeck }) => {
+const CardContainer = ({ card, index, prevIndex, mode, next, back, cardCount }) => {
 
   return(
     <div className='card-container'>
       <div className='inner-card-container'>
         <AnimatePresence exitBeforeEnter>
-          {mode === 'Study' && 
-            <Card
-              card={card}
-              key={card.id} 
-              index={index}
-              mode={mode} />
-          }
-          {mode === 'Quiz' &&
-            <Card 
+          <Card
             card={card}
             key={card.id} 
             index={index}
-            mode={mode} />
-          }
+            prevIndex={prevIndex}
+            mode={mode} 
+            back={back}
+            next={next}
+            cardCount={cardCount}
+          />
         </AnimatePresence>
       </div>
-
-      <div className='navigation-buttons'>
-        <button className='nav-button back-button' onClick={back}><FontAwesomeIcon icon={faArrowLeft} size='3x' /></button>
-        <div style={{backgroundColor: 'var(--red)', color: 'var(--darker)', padding: '.5rem', borderRadius: '.5rem', textAlign: 'center'}}>
-          <h4>Card Number</h4>
-          <div style={{margin: '.6rem 0', fontSize: '1.9rem', fontWeight: '600'}}>
-            <input className='card-nav-input'></input>
-            <span> / {cardsInDeck}</span>
-          </div>
-          <button style={{ display: 'inline-block', backgroundColor: 'var(--dark', color: 'var(--light'}} onHover={{color: 'white'}}>Go</button>
-        </div>
-        <button className='nav-button next-button' onClick={next}><FontAwesomeIcon icon={faArrowRight} size='3x' /></button>
-      </div>
+      {/* <CardNav back={back} next={next} cardCount={cardCount} setDirection={setDirection} /> */}
     </div>
   )
 }
