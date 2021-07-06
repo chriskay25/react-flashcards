a54e813b1464aab410b89aeceed1d0b1c2ff74e9
Chris Kay <ckay84@protonmail.com>
2021-07-05 23:59:25 -0400

    State for selected deck, deselect option when clicking outside

--- src/components/Decks.jsx
+++ src/components/Decks.jsx
@@ -1,57 +1,68 @@
-import React, { useEffect } from 'react'
-import { motion } from 'framer-motion'
+import React, { useEffect, useState, useRef } from 'react'
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion'
 import DeckOption from './DeckOption'
 import { getDecks } from '../actions/deckActions'
 import { useSelector, useDispatch } from 'react-redux'
 
+const optionVariants = {
+    hidden: {
+        x: '100vw',
+    },
+    visible: i => ({
+        x: 0,
+        transition: {
+            delay: i/4,
+            type: 'spring',
+            damping: 15,
+        }
+    }),
+}
+
 const Decks = () => {
-
-    const optionVariants = {
-        hidden: {
-            x: '100vw',
-        },
-        visible: i => ({
-            x: 0,
-            transition: {
-                delay: i/4,
-                type: 'spring',
-                damping: 15,
-            }
-        }),
-    }
-
+    const ref = useRef()
     const decks = useSelector(state => state.deckReducer.decks)
     const dispatch = useDispatch()
-    
+    const [selectedId, setSelectedId] = useState(null)
+
+    const handleClick = (id) => {
+        selectedId === id ? setSelectedId(null) : setSelectedId(id)
+    }
+
     useEffect(() => {
         dispatch(getDecks())
-    }, [dispatch])
 
-    const populate = () => {
-        return decks.map((deck, idx) => {
-            return (
-                <motion.div 
-                    className='deck-option-container' 
-                    key={deck.id} 
-                    variants={optionVariants} 
-                    custom={idx} 
-                    initial='hidden' 
-                    animate='visible'
-                    layout
-                >
-                    <DeckOption deck={deck} />
-                </motion.div>
-            )
-        })
-    }
+        const handleOutsideClick = (e) => {
+            if (ref.current.contains(e.target)) {
+                console.log('ref inside: ', ref)
+                return 
+            }
+            console.log('ref outside: ', ref)
+            setSelectedId(null)
+        }
+
+        document.addEventListener('mousedown', handleOutsideClick)
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick)
+        }
+    }, [dispatch])
 
     return (
         <div className='decks-container'>
-            <div className='decks-header'>
-                <h2>Decks</h2>
-            </div>
-            <div className='deck-options-container'>
-                {populate()}
+            <h2 className='decks-header'>Decks</h2>
+            <div ref={ref} className='deck-options-container'>
+                <AnimateSharedLayout type='crossfade'>
+                    {decks.map(deck => (
+                        <motion.div 
+                            className='deck-option-container' 
+                            key={deck.id} 
+                            layoutId={deck.id} 
+                            onClick={() => handleClick(deck.id)}
+                        >
+                            <DeckOption deck={deck} selected={selectedId === deck.id} setSelectedId={setSelectedId} />
+                        </motion.div>
+                    ))}
+                </AnimateSharedLayout>
             </div>
         </div>
     )
