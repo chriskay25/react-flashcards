8e6aae0ba6942343ea8f285965096d567fd64dc9
Chris Kay <ckay84@protonmail.com>
2021-07-05 23:55:25 -0400

    Change menu layout and close when clicking outside, need to fix close bttn though

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,8 @@
-import { React, useState } from 'react'
+import { React, useEffect, useRef } from 'react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouseUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
 const menuVariants = {
@@ -12,45 +11,61 @@ const menuVariants = {
     },
     visible: {
         opacity: 0,
-        scaleY: 0,
+        x: '200px',
+        transition: {
+            type: 'spring',
+            stiffness: 175,
+            damping: 25,
+        }
     },
     open: {
-        originY: '100px',
         opacity: 1,
-        scaleY: 1,
+        x: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 175,
+            damping: 25,
+        }
     }
 }
 
-const Menu = ({ logoutUser }) => {
-    const [open, setOpen] = useState(false)
+const Menu = ({ logoutUser, open, setOpen }) => {
+    const ref = useRef()
 
-    const handleMenuClick = () => {
-        setOpen(!open)
+    const handleClick = (e) => {
+        if (ref.current.contains(e.target)) {
+            return 
+        }
+        setOpen()
     }
 
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClick)
+
+        return () => {
+            document.removeEventListener('mousedown', handleClick)
+        }
+    }, [])
+
     return (
-        <div className='menu-container'>
-            <div className='menu-button'>
-                <div className='fontawesome-bars' onClick={handleMenuClick}>
-                    {open ? <FontAwesomeIcon icon={faTimes} size='2x' /> : <FontAwesomeIcon icon={faBars} size='2x' />}
-                </div>
-            </div>
+        <motion.div ref={ref} className='menu-container' layout>
             <motion.div
                 className='menu'
                 variants={menuVariants}
                 initial='hidden'
                 animate={open ? 'open' : 'visible'}
                 layout
-            >
+            >   
                 <Link to='/' className='fontawesome-home'>
-                    <FontAwesomeIcon icon={faHouseUser} size='2x' />
+                    <FontAwesomeIcon icon={faHouseUser} />
+                    <span style={{padding: '1rem'}}>Home</span>
                 </Link>
-
-                <div className='fontawesome-logout' onClick={logoutUser}>
-                    <FontAwesomeIcon icon={faSignOutAlt} size='2x' />
+                <div style={{marginTop: '2rem'}} className='fontawesome-logout' onClick={logoutUser}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                    <span style={{padding: '1rem'}}>Logout</span>
                 </div>
             </motion.div>
-        </div>
+        </motion.div>
     )
 }
 
