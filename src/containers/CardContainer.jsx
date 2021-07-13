bba7837dbdc37fbdb2b79a16ba8151e166e757a9
Chris Kay <ckay84@protonmail.com>
2021-07-13 16:18:09 -0400

    Remove unused div

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
 import Card from '../components/Card'
-// import CardNav from '../components/CardNav'
 import { AnimatePresence } from 'framer-motion'
 
-const CardContainer = ({ card, index, prevIndex, mode, next, back, cardCount }) => {
+const CardContainer = ({ card, index, prevIndex, mode, next, back, goTo, cardCount }) => {
 
   return(
     <div className='card-container'>
-      <div className='inner-card-container'>
-        <AnimatePresence exitBeforeEnter>
-          <Card
-            card={card}
-            key={card.id} 
-            index={index}
-            prevIndex={prevIndex}
-            mode={mode} 
-            back={back}
-            next={next}
-            cardCount={cardCount}
-          />
-        </AnimatePresence>
-      </div>
-      {/* <CardNav back={back} next={next} cardCount={cardCount} setDirection={setDirection} /> */}
+      <AnimatePresence exitBeforeEnter>
+        <Card
+          card={card}
+          key={card.id} 
+          index={index}
+          prevIndex={prevIndex}
+          mode={mode} 
+          back={back}
+          next={next}
+          goTo={goTo}
+          cardCount={cardCount}
+        />
+      </AnimatePresence>
     </div>
   )
 }
