719590eeac7106a1042efa1885fe7ec9536cab42
Chris Kay <ckay84@protonmail.com>
2021-07-13 16:45:54 -0400

    Add an action to start a quiz session

---
+++ src/actions/sessionActions.js
@@ -0,0 +1,24 @@
+export const startSession = (deck) => {
+    return (dispatch) => {
+
+        let cards = deck.cards.map(card => {
+            return Object.assign({}, card, {
+              answered: false,
+              correct: null
+            })
+        })
+
+        const deckObject = {
+            deck: {
+              id: deck.id,
+              name: deck.name
+            },
+            cards: cards
+        }
+
+        dispatch({
+            type: 'START',
+            payload: deckObject
+        })
+    }
+}
