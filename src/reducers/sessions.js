e38daa5b5ae4cf34612d4db64fd50bb68ae276f5
Chris Kay <ckay84@protonmail.com>
2021-07-13 17:11:33 -0400

    Set answered state for individual cards

--- src/reducers/sessions.js
+++ src/reducers/sessions.js
@@ -1,3 +1,13 @@
+const updateCardInArray = (array, cardId) => {
+    const updatedCards = array.map(card => {
+      if (card.id !== cardId) return card
+      card.answered = true
+      return card
+    })
+  
+    return updatedCards
+}
+
 const initialState = {
     started: false,
     finished: false,
@@ -6,12 +16,18 @@ const initialState = {
 export const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'START':
-      return {
-        ...state,
-        started: true,
-        deck: action.payload.deck,
-        cards: action.payload.cards
-      };
+        return {
+            ...state,
+            started: true,
+            deck: action.payload.deck,
+            cards: action.payload.cards
+        };
+    case 'ANSWERED':
+        const newCards = updateCardInArray(state.cards, action.payload)
+        return {
+            ...state,
+            cards: newCards
+        };
     default:
       return state;
   }
