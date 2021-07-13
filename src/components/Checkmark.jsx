e96fb663a6663d98318f96a2792e0df2c7a30a6f
Chris Kay <ckay84@protonmail.com>
2021-07-13 16:29:42 -0400

    Set svg to display a circle with an X or checkmark on answer submission

--- src/components/Checkmark.jsx
+++ src/components/Checkmark.jsx
@@ -1,54 +1,66 @@
 import React from 'react'
 import { motion } from 'framer-motion'
- 
-const checkmarkVariants = {
+
+const circle = {
+    hidden: {
+        strokeDashoffset: 160,
+        rotate: -90,
+    },
+    visible: {
+        strokeDashoffset: 0,
+        transition: {
+            duration: .6,
+            delay: .2,
+            // ease: 'easeInOut',
+        }
+    }
+}
+
+const check = {
     hidden: {
         pathLength: 0,
-        opacity: 0
+        opacity: 0,
     },
     visible: {
         opacity: 1,
         pathLength: 1,
         transition: {
-            duration: 1,
+            delay: .8,
+            duration: .8,
             ease: 'easeInOut',
-            // delay: .5,
         }
-    },
-    exit: {pathLength: 0}
+    }
 }
 
-const Checkmark = () => {
+const Checkmark = ({ correct }) => {
+
+    const checkPath = () => {
+        return (
+            <motion.path className="check" 
+                d="M14.1 27.2l7.1 7.2 16.7-16.8" 
+                initial='hidden' 
+                animate='visible' 
+                variants={check} 
+            />
+        )
+    }
+
+    const xPath = () => {
+        return (
+            <>
+                <motion.path className="check" d="m 14 14 l 24 24" initial='hidden' animate='visible' variants={check} />
+                <motion.path className="check" d="m 14 38 l 24 -24" initial='hidden' animate='visible' variants={check} />
+            </>
+        )
+    }
 
     return (
-        <motion.svg className='checkmark' 
-            xlmns='http://www.w3.org/2000/svg' 
-            viewBox='0 0 120 25'
-        >
-            <motion.path
-                variants={checkmarkVariants}
-                initial='hidden'
-                animate='visible'
-                exit='exit'
-                // d="M 0 32 L 32 64 L 85 0"
-                d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-                fill="transparent"
-                strokeWidth="2"
-                stroke='var(--lightblue)'
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            /> 
-            <motion.polyline 
-                fill='none' 
-                stroke='var(--green)'
-                strokeWidth='3'
-                strokeLinecap="round"
-                strokeLinejoin='round'
-                initial={{opacity: 0, points: '10 10 10 10.01 10 10.01'}}
-                animate={{opacity: 1, points: "22 4 12 15.01 9 9.01", transition: {delay: .8, duration: .8, ease: 'easeInOut'}}}
-                exit={{points: '10 10 10 10 10 10'}}
-            />
-        </motion.svg>
+        <div style={{width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <svg className={correct ? 'checkmark correct' : 'checkmark incorrect'} viewBox="0 0 52 52">
+                <motion.circle layout className="checkmark-circle" cx='26' cy='26' r='25' initial='hidden' animate='visible' variants={circle}/>
+                {correct ? checkPath() : xPath()}
+            </svg>
+        </div>
     )
 }
 
