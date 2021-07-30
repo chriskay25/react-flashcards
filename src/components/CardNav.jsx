8b8652264746840896dd70e4c975c49fa8adf1b1
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-07-30 15:59:15 -0400

    Set direction on hover for buttons, and onChange for input, patchwork solution for now

--- src/components/CardNav.jsx
+++ src/components/CardNav.jsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-const CardNav = ({ back, next, goTo, cardCount, prevIndex, index, setDirection }) => {
+const CardNav = ({ back, next, goTo, cardCount, index, setDirection, id }) => {
     const [navIndex, setNavIndex] = useState(null)
 
+    const handleDirection = (dir) => {
+        setNavIndex(null)
+        setDirection(dir)
+    }
+
     const handleNext = () => {
-        setDirection('forward')
         next()
     }
 
     const handleBack = () => {
-        setDirection('backward')
         back()
     }
 
     const handleNavInput = (e) => {
         setNavIndex(e.target.value)
+        navIndex > index ? setDirection('forward') : setDirection('backward')
     }
 
     const handleNavSubmit = () => {
-        navIndex > prevIndex ? setDirection('forward') : setDirection('backward')
-        goTo(navIndex)
+        // navIndex > index ? setDirection('forward') : setDirection('backward')
+        navIndex && goTo(navIndex)
     }
 
     return (
         <div className='navigation-buttons'>
-            <button className='nav-button back-button' onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} size='3x' /></button>
+            <motion.button className='nav-button back-button' whileHover={() => handleDirection('backward')} onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} size='3x' /></motion.button>
             <div className='card-nav'>
                 <h4>Card Number</h4>
                 <div className='inner-card-nav'>
@@ -35,7 +40,7 @@ const CardNav = ({ back, next, goTo, cardCount, prevIndex, index, setDirection }
                     <button onClick={() => handleNavSubmit()} style={{ marginLeft: '1rem', backgroundColor: 'var(--dark)', color: 'var(--light'}}>Go</button>
                 </div>
             </div>
-            <button className='nav-button next-button' onClick={handleNext}><FontAwesomeIcon icon={faArrowRight} size='3x' /></button>
+            <motion.button className='nav-button next-button' whileHover={() => handleDirection('forward')} onClick={handleNext}><FontAwesomeIcon icon={faArrowRight} size='3x' /></motion.button>
         </div>
     )
 }
