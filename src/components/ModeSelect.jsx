94405e4b1e38b528091b0a920c7d1e4712c412e2
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-07-29 23:57:23 -0400

    Change the appearance of mode select, add confirmation bttn

--- src/components/ModeSelect.jsx
+++ src/components/ModeSelect.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import { motion } from 'framer-motion'
+import React, { useState } from 'react';
+import { motion, AnimateSharedLayout } from 'framer-motion'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-const modeSelectVariants = {
-  hidden: {
-    opacity: 0,
-    scale: 0,
-  },
-  visible: {
-    scale: 1,
-    opacity: 1, 
-    transition: {
-      delay: .3,
-      type: 'spring',
-      stiffness: 150,
-    }
-  },
-  hover: {
-    boxShadow: '0 0 0 4px var(--light)',
-    backgroundColor: 'var(--dark)',
-    color: 'var(--lightcoral)',
-  },
-  tap: {
-    boxShadow: '0 0 0 4px var(--light)',
-    backgroundColor: 'var(--dark)',
-    color: 'var(--lightcoral)',
-  }
-}
+const ModeSelect = ({ handleModeSelect }) => {
+    const [selected, setSelected] = useState(null)
+    const modes = ['quiz', 'study']
 
-const ModeSelect = ({ handleClick }) => {
     return (
-        <span className='mode-select'>
-            <h2>mode</h2>
-            <motion.button onClick={handleClick} variants={modeSelectVariants} initial='hidden' animate='visible' whileHover='hover' whileTap='tap'>Study</motion.button> 
-            <motion.button onClick={handleClick} variants={modeSelectVariants} initial='hidden' animate='visible' whileHover='hover' whileTap='tap'>Quiz</motion.button> 
-        </span>
+        <div className='mode-select-container'>
+            <h2>Select Mode</h2>
+            <div className='mode-select'>
+
+                <AnimateSharedLayout>
+                    {modes.map((mode) => (
+                        <motion.span className={`motion-option ${selected === mode && "selected"}`}
+                            key={mode} 
+                            onClick={() => setSelected(mode)}
+                            layout
+                        >
+                            {selected === mode && (
+                                <motion.span
+                                    className='underline'
+                                    layoutId='underline'
+                                />
+                            )}
+                            {mode}
+                        </motion.span>
+                    ))}
+                </AnimateSharedLayout>
+            </div>
+            <div className='confirm-mode'>
+                {selected && <button onClick={() => handleModeSelect(selected)}>{<FontAwesomeIcon icon={faArrowRight} size={'2x'} />}</button>}
+            </div>
+        </div>
     )
 }
 
