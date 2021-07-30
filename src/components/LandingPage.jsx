2c48e4713ebab51056bdcf5b999ba3d40000e390
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-07-29 23:59:00 -0400

    Convert to functional component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
-class LandingPage extends Component {
-    render() {
-        return (
-            <motion.div className='landing-page' animate={{opacity: 1}} transition={{delay: .5}}>
-                <Link to='/login'>Log In</Link>
-                <Link to='/signup'>Sign Up</Link>
-            </motion.div>
-        )
-    }
+const LandingPage = () => {
+    return (
+        <motion.div className='landing-page' animate={{opacity: 1}} transition={{duration: .2}}>
+            <Link to='/login'>Log In</Link>
+            <Link to='/signup'>Sign Up</Link>
+        </motion.div>
+    )
 }
 
 export default LandingPage
