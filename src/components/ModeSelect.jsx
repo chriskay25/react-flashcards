e1b45bb5dffb50981da676a262966d9892a23dc6
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-06 00:00:05 -0500

    Make component compatible with its new position in DeckOptions

--- src/components/ModeSelect.jsx
+++ src/components/ModeSelect.jsx
@@ -1,40 +1,57 @@
-import React, { useState } from 'react';
-import { motion, AnimateSharedLayout } from 'framer-motion'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { modeSelect, startSession } from "../actions/sessionActions";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-const ModeSelect = ({ handleModeSelect }) => {
-    const [selected, setSelected] = useState(null)
-    const modes = ['quiz', 'study']
+const ModeSelect = ({ deck }) => {
+  const [selected, setSelected] = useState("quiz");
+  const [redirected, setRedirected] = useState(false);
+  const modes = ["quiz", "study", "edit"];
+  const dispatch = useDispatch();
 
-    return (
-        <div className='mode-select-container'>
-            <h2>Select Mode</h2>
-            <div className='mode-select'>
+  const handleModeSelect = () => {
+    dispatch(modeSelect(selected));
+    dispatch(startSession(deck));
+    setRedirected(true);
+  };
 
-                <AnimateSharedLayout>
-                    {modes.map((mode) => (
-                        <motion.span className={`motion-option ${selected === mode && "selected"}`}
-                            key={mode} 
-                            onClick={() => setSelected(mode)}
-                            layout
-                        >
-                            {selected === mode && (
-                                <motion.span
-                                    className='underline'
-                                    layoutId='underline'
-                                />
-                            )}
-                            {mode}
-                        </motion.span>
-                    ))}
-                </AnimateSharedLayout>
-            </div>
-            <div className='confirm-mode'>
-                {selected && <button onClick={() => handleModeSelect(selected)}>{<FontAwesomeIcon icon={faArrowRight} size={'2x'} />}</button>}
-            </div>
-        </div>
-    )
-}
+  return (
+    <motion.div
+      className="mode-select-container"
+      layout
+      initial={{ opacity: 0, scale: 0.4 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ delay: 0.4, type: "spring" }}
+    >
+      <h2>Select Mode</h2>
+      <div className="mode-select">
+        <AnimateSharedLayout>
+          {modes.map((mode) => (
+            <motion.div
+              className={`mode-option ${selected === mode && "selected"}`}
+              key={mode}
+              onClick={() => setSelected(mode)}
+              layout
+            >
+              {selected === mode && (
+                <motion.span className="underline" layoutId="underline" />
+              )}
+              {mode}
+            </motion.div>
+          ))}
+        </AnimateSharedLayout>
+      </div>
+      <div className="confirm-mode">
+        <button onClick={() => handleModeSelect(selected)}>
+          {<FontAwesomeIcon icon={faArrowRight} size={"2x"} />}
+        </button>
+      </div>
+      {redirected && <Redirect to={{ pathname: `/decks/${deck.id}` }} />}
+    </motion.div>
+  );
+};
 
-export default ModeSelect
+export default ModeSelect;
