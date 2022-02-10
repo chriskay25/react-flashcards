9ef3d148bfe7f6729cd4055d2b8d758c7ab0d55d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-10 00:04:52 -0500

    Prettier auto formatting

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,35 +1,55 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { userLogin } from '../actions/userActions'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { userLogin } from "../actions/userActions";
 
 const LoginForm = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const dispatch = useDispatch()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value)
-    }
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
-    }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatch(userLogin({ username, password }))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(userLogin({ username, password }));
+  };
 
-    return (
-        <div>
-            <form className='auth-form' onSubmit={handleSubmit}>
-                <h2 className='auth-form-title'>LOGIN</h2>
-                <input type='text' name='username' value={username} placeholder='Username' onChange={handleUsernameChange} autoFocus={true} />
-                <input type='text' name='password' value={password} placeholder='Password' onChange={handlePasswordChange} />
-                <input className='form-button' type='submit' value='Submit' />
-            </form>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90%",
+      }}
+    >
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <h2 className="auth-form-title">LOGIN</h2>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          placeholder="Username"
+          onChange={handleUsernameChange}
+          autoFocus={true}
+        />
+        <input
+          type="text"
+          name="password"
+          value={password}
+          placeholder="Password"
+          onChange={handlePasswordChange}
+        />
+        <input className="form-button" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
