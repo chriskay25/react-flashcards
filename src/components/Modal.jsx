f29b609ec3176ef09bdfda15dcca3266ef29e3d7
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-10 14:56:59 -0400

    Create Modal component to display hints and other potential msgs/alerts

---
+++ src/components/Modal.jsx
@@ -0,0 +1,50 @@
+import { useDispatch } from "react-redux";
+import { triggerHintModal } from "../actions/sessionActions";
+import { motion } from "framer-motion";
+
+const Modal = ({ content }) => {
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    const modalObject = { open: false, content: null };
+    dispatch(triggerHintModal(modalObject));
+  };
+
+  return (
+    <div className="modal-container">
+      <div style={{ height: "100%", width: "100%", position: "relative" }}>
+        <motion.div
+          className="modal"
+          initial={{ scale: 0.3, opacity: 0 }}
+          animate={{
+            scale: 1,
+            opacity: 1,
+            transition: { ease: "easeInOut", duration: 0.3 },
+          }}
+        >
+          <button
+            style={{
+              position: "absolute",
+              top: 5,
+              right: 8,
+              background: "transparent",
+              border: "none",
+              fontSize: "2rem",
+            }}
+            onClick={handleClose}
+          >
+            X
+          </button>
+          <h3
+            style={{ textAlign: "center", marginTop: "10px", fontSize: "2rem" }}
+          >
+            Hint
+          </h3>
+          <p style={{ fontSize: "1.5rem", marginTop: "20px" }}>{content}</p>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
