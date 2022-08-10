5242e492d237cda5d0faee8fd1aec98968b2fc44
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-10 14:46:05 -0400

    No real changes, edits by prettier

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,87 +1,87 @@
 export const getCurrentUser = () => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      return (dispatch) => {
-        fetch('http://localhost:3000/api/v1/auth', {
-          headers: {
-            'Authorization': token,
+  const token = localStorage.getItem("token");
+  if (token) {
+    return (dispatch) => {
+      fetch("http://localhost:3000/api/v1/auth", {
+        headers: {
+          Authorization: token,
+        },
+      })
+        .then((resp) => resp.json())
+        .then((user) => {
+          if (user.error) {
+            alert(user.error);
+          } else {
+            dispatch({ type: "GET_CURRENT_USER", payload: user });
           }
-        })
-          .then(resp => resp.json())
-          .then(user => {
-            if (user.error) {
-              alert(user.error)
-            } else {
-              dispatch({type: 'GET_CURRENT_USER', payload: user})
-            }
-          })
-      }
-    } else {
-      return (dispatch) => {
-        dispatch({type: 'GET_CURRENT_USER', payload: null})
-      }
-    }
+        });
+    };
+  } else {
+    return (dispatch) => {
+      dispatch({ type: "GET_CURRENT_USER", payload: null });
+    };
   }
+};
 
 export const userLogin = (data) => {
-    return (dispatch) => {
-        fetch('http://localhost:3000/api/v1/login', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                user: data
-            })
-        })
-            .then(resp => resp.json())
-            .then(userData => {
-                if (userData.error) {
-                    alert('Invalid Credentials')
-                } else {
-                    localStorage.setItem('token', userData.jwt)
-                    dispatch({
-                        type: 'LOGIN',
-                        payload: userData.user
-                    })
-                }
-
-            })
-    }
-}
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        user: data,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((userData) => {
+        if (userData.error) {
+          alert("Invalid Credentials");
+        } else {
+          localStorage.setItem("token", userData.jwt);
+          dispatch({
+            type: "LOGIN",
+            payload: userData.user,
+          });
+        }
+      });
+  };
+};
 
 export const userLogout = () => {
-  localStorage.clear()
+  localStorage.clear();
   return (dispatch) => {
     dispatch({
-      type: 'LOGOUT',
-      payload: null 
-    })
-  }
-}
+      type: "LOGOUT",
+      payload: null,
+    });
+  };
+};
 
 export const userSignup = (data) => {
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/signup', {
+    fetch("http://localhost:3000/api/v1/signup", {
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        user: data
-      })
+        user: data,
+      }),
     })
-      .then(resp => resp.json())
-      .then(userData => {
+      .then((resp) => resp.json())
+      .then((userData) => {
+        debugger;
         if (userData.error) {
-            alert('Invalid Credentials')
+          alert("Invalid Credentials");
         } else {
-            localStorage.setItem('token', userData.jwt)
-            dispatch({
-                type: 'LOGIN',
-                payload: userData.user
-            })
+          localStorage.setItem("token", userData.jwt);
+          dispatch({
+            type: "LOGIN",
+            payload: userData.user,
+          });
         }
-      })
-  }
-}
+      });
+  };
+};
