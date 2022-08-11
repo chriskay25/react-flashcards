1676812110a74039fa43401d14af562b062406fb
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-10 23:21:47 -0400

    Replace extracted code w/ MenuButton

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,76 +1,80 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { motion } from 'framer-motion'
-import { useDispatch } from 'react-redux'
-import { userLogout } from '../actions/userActions'
-import Menu from './Menu'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { userLogout } from "../actions/userActions";
+import Menu from "./Menu";
+import MenuButton from "./MenuButton";
 
 const navBarVariants = {
   hidden: {
-    height: '25px',
+    height: "25px",
   },
   visible: {
-    height: [null, '50px'],
-    justifyContent: 'center',
+    height: [null, "50px"],
+    justifyContent: "center",
   },
   loggedIn: {
-    height: '25px',
-    justifyContent: 'space-between',
+    height: "25px",
+    justifyContent: "space-between",
     transition: {
       duration: 1,
-      type: 'spring',
-    }
-  }
-}
+      type: "spring",
+    },
+  },
+};
 
 const titleVariants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
-    fontSize: '40px',
+    fontSize: "40px",
   },
   loggedIn: {
-    fontSize: '20px',
+    fontSize: "20px",
     transition: {
-      duration: .5,
-    }
-  }
-}
+      duration: 0.5,
+    },
+  },
+};
 
 const NavBar = () => {
-  const currentUser = useSelector(state => state.userReducer.currentUser)
-  const dispatch = useDispatch()
-  const [open, setOpen] = useState(false)
+  const currentUser = useSelector((state) => state.userReducer.currentUser);
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
-  const handleMenuClick = () => {
-      if (!open) setOpen(true)
-  }
+  useEffect(() => {
+    return () => setOpen(false);
+  }, []);
 
   const logoutUser = () => {
-    dispatch(userLogout())
-  }
+    setOpen(false);
+    dispatch(userLogout());
+  };
 
   return (
-    <motion.div className='NavBar' layout initial='hidden' animate={currentUser ? 'loggedIn' : 'visible'} variants={navBarVariants}>
-      
-      <motion.div layout className='app-title' initial='hidden' animate={currentUser ? 'loggedIn' : 'visible'} variants={titleVariants}>
-        <div className='title'>FLASHCARDS</div>
+    <motion.div
+      className="NavBar"
+      layout
+      initial="hidden"
+      animate={currentUser ? "loggedIn" : "visible"}
+      variants={navBarVariants}
+    >
+      <motion.div
+        layout
+        className="app-title"
+        initial="hidden"
+        animate={currentUser ? "loggedIn" : "visible"}
+        variants={titleVariants}
+      >
+        <div className="title">FLASHCARDS</div>
       </motion.div>
 
-      {currentUser && 
-        <div className='menu-button' onClick={handleMenuClick}>
-          <div className='fontawesome-menu-button'>
-            {open ? <FontAwesomeIcon icon={faTimes} size='2x' /> : <FontAwesomeIcon icon={faBars} size='2x' />}
-          </div>
-        </div>
-      }
+      {currentUser && <MenuButton open={open} setOpen={setOpen} />}
 
-      <Menu logoutUser={logoutUser} open={open} setOpen={setOpen} />
-     
+      {open && <Menu logoutUser={logoutUser} open={open} setOpen={setOpen} />}
     </motion.div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
