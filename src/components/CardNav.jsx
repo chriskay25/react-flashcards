167dbd7b7f1561730f0d4d621360fe0f0db64a9b
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-14 00:47:20 -0400

    Remove direction related code, reset navIndex after actions

--- src/components/CardNav.jsx
+++ src/components/CardNav.jsx
@@ -1,48 +1,68 @@
-import React, { useState } from 'react'
-import { motion } from 'framer-motion'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-const CardNav = ({ back, next, goTo, cardCount, index, setDirection, id }) => {
-    const [navIndex, setNavIndex] = useState(null)
+const CardNav = ({ back, next, goTo, cardCount }) => {
+  const [navIndex, setNavIndex] = useState("");
 
-    const handleDirection = (dir) => {
-        setNavIndex(null)
-        setDirection(dir)
+  const handleNext = () => {
+    next();
+    setNavIndex("");
+  };
+
+  const handleBack = () => {
+    back();
+    setNavIndex("");
+  };
+
+  const handleNavInput = (e) => {
+    let verifyInt = parseInt(e.target.value);
+    if (isNaN(verifyInt)) {
+      setNavIndex("");
+      return;
     }
+    setNavIndex(parseInt(e.target.value));
+  };
 
-    const handleNext = () => {
-        next()
-    }
+  const handleNavSubmit = () => {
+    navIndex && goTo(navIndex);
+    setNavIndex("");
+  };
 
-    const handleBack = () => {
-        back()
-    }
-
-    const handleNavInput = (e) => {
-        setNavIndex(e.target.value)
-        navIndex > index ? setDirection('forward') : setDirection('backward')
-    }
-
-    const handleNavSubmit = () => {
-        // navIndex > index ? setDirection('forward') : setDirection('backward')
-        navIndex && goTo(navIndex)
-    }
-
-    return (
-        <div className='navigation-buttons'>
-            <motion.button className='nav-button back-button' whileHover={() => handleDirection('backward')} onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} size='3x' /></motion.button>
-            <div className='card-nav'>
-                <h4>Card Number</h4>
-                <div className='inner-card-nav'>
-                    <input onChange={handleNavInput} className='card-nav-input'></input>
-                    <span> / {cardCount}</span>
-                    <button onClick={() => handleNavSubmit()} style={{ marginLeft: '1rem', backgroundColor: 'var(--dark)', color: 'var(--light'}}>Go</button>
-                </div>
-            </div>
-            <motion.button className='nav-button next-button' whileHover={() => handleDirection('forward')} onClick={handleNext}><FontAwesomeIcon icon={faArrowRight} size='3x' /></motion.button>
+  return (
+    <div className="navigation-buttons">
+      <motion.button className="nav-button back-button" onClick={handleBack}>
+        <FontAwesomeIcon icon={faArrowLeft} size="3x" />
+      </motion.button>
+      <div className="card-nav">
+        <h4>Card Number</h4>
+        <div className="inner-card-nav">
+          <input
+            style={{ color: "white" }}
+            value={navIndex}
+            onChange={handleNavInput}
+            className="card-nav-input"
+          ></input>
+          <span> / {cardCount}</span>
+          <button
+            onClick={() => handleNavSubmit()}
+            style={{
+              marginLeft: "1rem",
+              backgroundColor: "var(--dark)",
+              color: "var(--light",
+              border: "none",
+            }}
+          >
+            Go
+          </button>
         </div>
-    )
-}
+      </div>
+      <motion.button className="nav-button next-button" onClick={handleNext}>
+        <FontAwesomeIcon icon={faArrowRight} size="3x" />
+      </motion.button>
+    </div>
+  );
+};
 
-export default CardNav
+export default CardNav;
