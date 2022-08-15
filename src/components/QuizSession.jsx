99e929b7b633d2b948a93b8b0ca2a2a444688e75
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-15 00:43:00 -0400

    Create QuizSession component

---
+++ src/components/QuizSession.jsx
@@ -0,0 +1,26 @@
+import CardContainer from "../containers/CardContainer";
+import CardNav from "./CardNav";
+
+const QuizSession = ({ session, index, back, next, goTo }) => {
+  return (
+    <>
+      <CardContainer
+        card={session.cards[index]}
+        index={index}
+        mode={session.mode}
+        deckLength={session.cards.length}
+      />
+
+      <CardNav
+        back={back}
+        next={next}
+        cardCount={session.cards.length}
+        goTo={goTo}
+        index={index}
+        id={session.cards.length > 0 ? session.cards[index].id : 0}
+      />
+    </>
+  );
+};
+
+export default QuizSession;
