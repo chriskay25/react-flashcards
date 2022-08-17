d627085ea75e6d3749fffcc14e740d28424902e4
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-16 21:39:17 -0400

    Conditional rendering answer for quiz and study modes

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { questionAnswered } from '../actions/sessionActions'
-import { motion } from 'framer-motion'
-import { TextareaAutosize } from '@material-ui/core'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { questionAnswered } from "../actions/sessionActions";
+import { motion } from "framer-motion";
+import { TextareaAutosize } from "@material-ui/core";
 
 const answerVariants = {
   hidden: {
-    opacity: 0
+    opacity: 0,
   },
   visible: {
-    opacity: 1
-  }
-}
+    opacity: 1,
+  },
+};
 
 const Answer = ({ answer, mode, answered, id }) => {
-  const [userAnswer, setUserAnswer] = useState('')
-  const dispatch = useDispatch()
+  const [userAnswer, setUserAnswer] = useState("");
+  const dispatch = useDispatch();
 
   const handleAnswerChange = (e) => {
-    setUserAnswer(e.target.value)
-  }
+    setUserAnswer(e.target.value);
+  };
 
   const handleAnswerSubmit = (e) => {
     const payload = {
-        id: id,
-        answer: userAnswer
-    }
-    dispatch(questionAnswered(payload))
-  }
+      id: id,
+      answer: userAnswer,
+    };
+    dispatch(questionAnswered(payload));
+  };
 
   return (
-    <motion.div className='answer' variants={answerVariants} initial='hidden' animate='visible' layout>
-      {mode === 'quiz' ? <TextareaAutosize value={userAnswer} onChange={handleAnswerChange} placeholder='Answer'></TextareaAutosize> : answer}
-      <button className='answer-submit' onClick={handleAnswerSubmit} disabled={answered}>Submit</button>
+    <motion.div
+      className="answer"
+      variants={answerVariants}
+      initial="hidden"
+      animate="visible"
+      layout
+    >
+      {mode === "quiz" && (
+        <>
+          <TextareaAutosize
+            value={userAnswer}
+            onChange={handleAnswerChange}
+            placeholder="Answer"
+          ></TextareaAutosize>
+          <button
+            className="answer-submit"
+            onClick={handleAnswerSubmit}
+            disabled={answered}
+          >
+            Submit
+          </button>
+        </>
+      )}
+      {mode === "study" && <div>{answer}</div>}
     </motion.div>
-  )
-}
+  );
+};
 
-export default Answer
+export default Answer;
