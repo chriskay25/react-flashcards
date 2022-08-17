87bd917cffcc89f2ec01398279bf8461a400229f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-16 21:53:26 -0400

    Add buttons to change deck modes

--- src/components/DeckTitle.jsx
+++ src/components/DeckTitle.jsx
@@ -1,16 +1,52 @@
+import { useDispatch } from "react-redux";
+import { modeSelect } from "../actions/sessionActions";
+
 const DeckTitle = ({ session }) => {
+  const dispatch = useDispatch();
+  const handleModeChange = (newMode) => {
+    dispatch(modeSelect(newMode));
+  };
+
   return (
     <div className="deck-title">
-      <h1>{session.deck.name}</h1>
-      <p
-        style={{
-          fontFamily: "Montserrat",
-          paddingLeft: "8px",
-          color: "lightsalmon",
-        }}
-      >
-        ({session.cards.length} Cards)
-      </p>
+      <div style={{ display: "flex", alignItems: "baseline" }}>
+        <h1>{session.deck.name}</h1>
+        <p
+          style={{
+            fontFamily: "Montserrat",
+            paddingLeft: "8px",
+            color: "lightsalmon",
+          }}
+        >
+          ({session.cards.length} Cards)
+        </p>
+      </div>
+      <div style={{}}>
+        {session.mode !== "quiz" && (
+          <button
+            onClick={() => handleModeChange("quiz")}
+            className="deck-title-button"
+          >
+            Quiz
+          </button>
+        )}
+        {session.mode !== "study" && (
+          <button
+            onClick={() => handleModeChange("study")}
+            className="deck-title-button"
+          >
+            Study
+          </button>
+        )}
+        {session.mode !== "edit" && (
+          <button
+            onClick={() => handleModeChange("edit")}
+            className="deck-title-button"
+          >
+            Edit
+          </button>
+        )}
+      </div>
     </div>
   );
 };
