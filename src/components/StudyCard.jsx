b16dc0054f73ce3efb47606ff5b7d163cf513eb2
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-08-22 20:18:16 -0400

    Make study cards flow into view

--- src/components/StudyCard.jsx
+++ src/components/StudyCard.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+const itemVariants = {
+  initial: { opacity: 0, y: "200px" },
+  visible: {
+    opacity: 1,
+    y: "0px",
+    transition: { type: "spring", damping: 10, stiffness: 40 },
+  },
+};
 
 const StudyCard = ({ card }) => {
   const [flip, setFlip] = useState(false);
   return (
-    <li
-      className={`study-card ${flip ? "flip" : ""}`}
-      onClick={() => setFlip(!flip)}
-    >
-      <div className="front">
-        <p style={{ padding: "10px" }}>{card.question}</p>
+    <motion.li variants={itemVariants} onClick={() => setFlip(!flip)}>
+      <div className={`study-card ${flip ? "flip" : ""}`}>
+        <div className="front">
+          <p style={{ fontSize: "2rem" }}>{card.question}</p>
+        </div>
+        <div className="back">
+          <p style={{ fontSize: "2rem" }}>{card.answer}</p>
+        </div>
       </div>
-      <div className="back">
-        <p>{card.answer}</p>
-      </div>
-    </li>
+    </motion.li>
   );
 };
 
--- src/components/StudyMode.jsx
+++ src/components/StudyMode.jsx
@@ -1,10 +1,31 @@
 import StudyCard from "./StudyCard";
+import { motion } from "framer-motion";
 
-const StudyMode = ({ cards }) => {
+const listVariants = {
+  initial: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const StudyMode = ({ deck }) => {
   const view = () => {
-    return cards.map((card) => <StudyCard key={card.id} card={card} />);
+    return deck.cards.map((card) => <StudyCard key={card.id} card={card} />);
   };
-  return <ul className="card-list study">{view()}</ul>;
+  return (
+    <motion.ul
+      className="card-list study"
+      initial="initial"
+      animate="visible"
+      variants={listVariants}
+      layout
+    >
+      {view()}
+    </motion.ul>
+  );
 };
 
 export default StudyMode;
